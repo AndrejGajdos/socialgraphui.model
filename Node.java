39bd9600739c4f76155a135106c66cf96411bee4
Andrej Gajdos <agajdy@gmail.com>
2014-12-26 20:53:22 +0100

    name of package update

--- Node.java
+++ Node.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cz.ario.socialgraphui.model;
+package socialgraphui.model;
 
 import java.util.Calendar;
 import java.util.Collection;
